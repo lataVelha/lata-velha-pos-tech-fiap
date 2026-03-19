bac135d6b1ae2a448fd237e4958237ac95c94b99
unknown <astek724@gmail.com>
2026-03-19 00:19:47 -0300

    projeto

---
+++ src/main/java/br/com/lata/velha/Lata/Velha/LataVelhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lata.velha.Lata.Velha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LataVelhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LataVelhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lata/velha/Lata/Velha/LataVelhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lata.velha.Lata.Velha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LataVelhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
