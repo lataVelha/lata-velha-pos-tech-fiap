29011248afa0fadbf79cb6d73cc8c52d71e4da5b
9903416 <9903416@prestadorserv.com.br>
2026-03-26 12:58:28 -0300

    refactor: separar BuscarProprietario em dois use cases
    - BuscarProprietarioPorIdUseCase: busca por ID
    - BuscarProprietarioPorDocumentoUseCase: busca por CPF/CNPJ
    - padronizado @Component + @RequiredArgsConstructor em todos use cases
    - controller atualizado com as novas injeções
    - removido BuscarProprietarioUseCase antigo

--- src/main/java/br/com/lata/velha/application/usecase/proprietario/BuscarProprietarioPorIdUseCase.java
+++ src/main/java/br/com/lata/velha/application/usecase/proprietario/BuscarProprietarioPorIdUseCase.java
@@ -1,27 +1,21 @@
 package br.com.lata.velha.application.usecase.proprietario;
 
 import br.com.lata.velha.application.assembler.ProprietarioAssembler;
-import br.com.lata.velha.application.dto.request.ProprietarioRequest;
 import br.com.lata.velha.application.dto.response.ProprietarioResponse;
 import br.com.lata.velha.domain.exception.ProprietarioNotFoundException;
-import br.com.lata.velha.domain.model.Proprietario;
 import br.com.lata.velha.domain.repository.ProprietarioRepository;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class AtualizarProprietarioUseCase {
+public class BuscarProprietarioPorIdUseCase {
 
     private final ProprietarioRepository repository;
     private final ProprietarioAssembler assembler;
 
-    public ProprietarioResponse execute(Long id, ProprietarioRequest request) {
-        Proprietario existente = repository.buscarPorId(id)
-                .orElseThrow(() -> new ProprietarioNotFoundException(id));
-        assembler.updateDomain(existente, request);
-        Proprietario salvo = repository.salvar(existente);
-        return assembler.toResponse(salvo);
+    public ProprietarioResponse execute(Long id) {
+        return assembler.toResponse(repository.buscarPorId(id)
+                .orElseThrow(() -> new ProprietarioNotFoundException(id)));
     }
 }
